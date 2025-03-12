0c0f730e0c408995b20655a3c7835c9c0a40e6c2
Qadar Yasin <qadaryasin24@gmail.com>
2025-03-13 00:31:39 +0500

    add some codes

---
+++ lab1.py
@@ -0,0 +1,59 @@
+# Comments in Python:(practice)
+
+x=1
+# the initial value of x is 1
+if x>0:
+   print("these are two comments") #print a string
+
+
+
+
+
+
+#Multiple Statements on a Single Line:(practice)
+
+print("Statement 1")
+print("Statement 2")
+
+# we can write both in a single line by using semicolon as:
+
+print("Statement 1"); print("Statement 2")
+
+
+
+
+#Indentation:(practice)
+
+x=1
+if x>0:
+print("This statement has no indentation") #ERROR OCCURED (there must be indentation)
+print("This statement has no indentation")
+
+
+#indentation is not fixed,but all statements have same indentation
+# single space 
+x=1
+if x>0:
+ print("This statement has a single space indentation") 
+ print("This statement has a single space indentation")
+
+# single tab 
+x=1
+if x>0:
+    print("This statement has a single tab indentation") 
+    print("This statement has a single tab indentation")
+
+#single space + single tab 
+x=1
+if x>0:
+     print("This statement has a single space+tab indentation") 
+     print("This statement has a single space+tab indentation")
+
+
+
+
+
+
+
+
+
