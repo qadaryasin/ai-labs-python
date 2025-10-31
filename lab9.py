369f1ae451118c31a5b719b455ad75cfcf997ada
Qadar Yasin <qadaryasin24@gmail.com>
2025-10-31 17:22:26 +0500

    Complete (Minimax, AlphabetaPruning Concepts)

--- lab9.py
+++ lab9.py
@@ -30,3 +30,61 @@ outcomes = [3, 5, 2, 9, 12, 5, 23, 23]
 maxDepth = int(math.log(len(outcomes), 2))
 
 print("The best value for MAX is:", minimax(0, 0, True, outcomes, maxDepth))
+
+
+
+# --------------------alphabetaPruning---------------------------
+
+import math
+
+
+def alphabeta(level, position, isMax, outcomes, maxDepth, alpha, beta):
+    # Base case: leaf node
+    if level == maxDepth:
+        return outcomes[position]
+
+    if isMax:  # MAX player's turn
+        best = -math.inf
+
+        # Left child
+        val = alphabeta(level+1, position*2, False,
+                        outcomes, maxDepth, alpha, beta)
+        best = max(best, val)
+        alpha = max(alpha, best)
+        if beta <= alpha:   # Prune
+            return best
+
+        # Right child
+        val = alphabeta(level+1, position*2+1, False,
+                        outcomes, maxDepth, alpha, beta)
+        best = max(best, val)
+        alpha = max(alpha, best)
+        return best
+
+    else:  # MIN player's turn
+        best = math.inf
+
+        # Left child
+        val = alphabeta(level+1, position*2, True,
+                        outcomes, maxDepth, alpha, beta)
+        best = min(best, val)
+        beta = min(beta, best)
+        if beta <= alpha:   # Prune
+            return best
+
+        # Right child
+        val = alphabeta(level+1, position*2+1, True,
+                        outcomes, maxDepth, alpha, beta)
+        best = min(best, val)
+        beta = min(beta, best)
+        return best
+
+
+# Leaf scores (outcomes of the game)
+outcomes = [3, 5, 2, 9, 12, 5, 23, 23]
+
+# Depth of tree
+maxDepth = int(math.log(len(outcomes), 2))
+
+print("The best value for MAX is:", alphabeta(
+    0, 0, True, outcomes, maxDepth, -math.inf, math.inf))
